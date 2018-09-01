23e0b093dd35355cd87c8cea65132748c1824536
Elsa Teklu <elizate56@gmail.com>
2018-09-01 09:30:57 +0300

    calendar api and entity #370

---
+++ Smart_Accounting/Smart_Accounting.Application/CalendarPeriods/Models/NewCalendarPeriodsModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Smart_Accounting.Application.CalendarPeriods.Models
+{
+    public class NewCalendarModel
+    {
+        public DateTime Start {get; set;}
+        public DateTime End {get; set;}
+    }
+}
