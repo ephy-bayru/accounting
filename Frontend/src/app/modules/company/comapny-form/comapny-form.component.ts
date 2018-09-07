1a32d0fff9338d722b7ffe0bb2f23ef855fbfd66
Mikael Araya <Mikaelaraya12@gmail.com>
2018-09-07 09:30:59 +0300

    #322 Organization Component finished

--- Frontend/src/app/modules/company/comapny-form/comapny-form.component.ts
+++ Frontend/src/app/modules/company/comapny-form/comapny-form.component.ts
@@ -9,6 +9,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Organization, CompanyService } from 'src/app/modules/company/company.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-comapny-form',
@@ -17,40 +19,76 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ComapnyFormComponent implements OnInit {
 
-
+  private organizationId: number;
   organizationForm: FormGroup;
-      constructor(private formBuilder: FormBuilder, private activatedRoute: ActivatedRoute) {
-             this.organizationForm = this.formBuilder.group({
-                Name: '',
-                Location: '',
-                Tin: ''
-              });
-      }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+    private organizationService: CompanyService, private activatedRoute: ActivatedRoute) {
+    this.organizationForm = this.formBuilder.group({
+      Name: '',
+      Location: '',
+      Tin: ''
+    });
   }
 
+  ngOnInit(): void {
+    this.organizationId = + this.activatedRoute.snapshot.paramMap.get('organizationId');
+    if (this.organizationId) {
+      this.organizationService.getOrganizationById(this.organizationId).subscribe(
+        (success: Organization) => this.populateForm(success),
+        (error: HttpErrorResponse) => this.handelResponse(error)
+      );
+    }
+  }
 
+  populateForm(organization: Organization) {
+    this.organizationForm.patchValue({
+      Name: organization.Name,
+      Location: organization.Location,
+      Tin: organization.Tin
+    });
+  }
   public focusIn(target: HTMLElement): void {
     target.parentElement.classList.add('e-input-focus');
-}
+  }
 
-public focusOut(target: HTMLElement): void {
+  public focusOut(target: HTMLElement): void {
     target.parentElement.classList.remove('e-input-focus');
-}
+  }
 
-public onMouseDown(target: HTMLElement): void {
+  public onMouseDown(target: HTMLElement): void {
     target.classList.add('e-input-btn-ripple');
-}
+  }
 
-public onMouseUp(target: HTMLElement): void {
+  public onMouseUp(target: HTMLElement): void {
     const ele: HTMLElement = target;
     setTimeout(
-            () => {ele.classList.remove('e-input-btn-ripple'); },
-            500);
-}
+      () => { ele.classList.remove('e-input-btn-ripple'); },
+      500);
+  }
 
-onSubmit()  {
-   }
+  prepareData(): Organization {
+    const form = this.organizationForm.value;
+    return {
+      Name: form.Name,
+      Location: form.Location,
+      Tin: form.Tin
+    };
+  }
+  handelResponse(response: any) {
+
+  }
+  onSubmit() {
+    const data = this.prepareData();
+    if (this.organizationId) {
+      this.organizationService.updateOrganization(this.organizationId, data).subscribe(
+        success => this.handelResponse(success),
+        error => this.handelResponse(error)
+      );
+    }
+    this.organizationService.createOrganization(data).subscribe(
+      success => this.handelResponse(success),
+      error => this.handelResponse(error)
+    );
+  }
 
 }
--- Frontend/src/app/modules/company/company.service.ts
+++ Frontend/src/app/modules/company/company.service.ts
@@ -9,6 +9,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class CompanyService {
@@ -19,40 +20,60 @@ export class CompanyService {
   }
   // Gets a single organization information by Id and returns an observable of organization
   getOrganizationById(id: number): Observable<Organization> {
-    return this.httpClient.get<Organization>(`${this.url}/${id}`);
+    return this.httpClient.get<Organization>(`${this.url}/${id}`)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   // Gets all the record of organization and returns and observable of Organization object
   getOrganizationsList(): Observable<Organization[]> {
-    return this.httpClient.get<Organization[]>(`${this.url}`);
+    return this.httpClient.get<Organization[]>(`${this.url}`)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   // gets organization based on the location and returns an aray of organizations found there
   getOrganizationByLocation(location: string): Observable<Organization[]> {
-    return this.httpClient.get<Organization[]>(`${this.url}/location`);
+    return this.httpClient.get<Organization[]>(`${this.url}/location`)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   // Creates a new instance of organization record in the system amd returns an observable
   // of the new organization information on success
   createOrganization(newOrganization: Organization): Observable<Organization> {
-    return this.httpClient.post<Organization>(`${this.url}`, newOrganization);
+    return this.httpClient.post<Organization>(`${this.url}`, newOrganization)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   // Update a single instance of organization record and returns a boolean depending on the success or
   // failure of the operation
   updateOrganization(id: number, updatedOrganization: Organization): Observable<boolean> {
-    return this.httpClient.put<boolean>(`${this.url}/${id}`, updatedOrganization);
+    return this.httpClient.put<boolean>(`${this.url}/${id}`, updatedOrganization)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   // deletes a single instance of organization record and returns boolean based on the outcome of the operation
   deleteOrganization(id: number): Observable<boolean> {
-    return this.httpClient.delete<boolean>(`${this.url}/${id}`);
+    return this.httpClient.delete<boolean>(`${this.url}/${id}`)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
-
+  private handleError(error: Response | any) {
+    return Observable.throw(error.status);
+  }
 }
 
-// blue print of ann organization data model id can be null depending on the operation at hand
+// blue print of an organization data model id can be null depending on the operation at hand
 // i.e for insert operation
 export class Organization {
   id?: number;
