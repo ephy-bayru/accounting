2d220195e5d3618408391ed141006bcd3995d941
ephrem <ephybayru@gmail.com>
2018-09-19 15:57:42 +0300

    updated employee query

--- Smart_Accounting/Smart_Accounting.Application/Employee/Queries/EmployeesQuery.cs
+++ Smart_Accounting/Smart_Accounting.Application/Employee/Queries/EmployeesQuery.cs
@@ -2,21 +2,25 @@ using System.Collections.Generic;
 using System.Linq;
 using Smart_Accounting.Application.Employee.Interfaces;
 using Smart_Accounting.Application.Interfaces;
-using Smart_Accounting.Domain.Employe;
+using Smart_Accounting.Domain.Employee;
 
 namespace Smart_Accounting.Application.Employee.Queries {
     public class EmployeesQuery : IEmployeesQueries {
 
         private IAccountingDatabaseService _database;
-        public EmployeesQuery(IAccountingDatabaseService database) {
+        public IEmployeeCommandsFactory _factory;
+        public EmployeesQuery(IAccountingDatabaseService database,
+                              IEmployeeCommandsFactory factory ) {
             _database = database;
+            _factory = factory;
         }
         public IEnumerable<Employees> GetAll () {
           return _database.Employees.ToList();
         }
 
-        public Employees GetById (uint employeeId) {
-            throw new System.NotImplementedException ();
+        public Employees GetById (uint id) {
+            var employee = _database.Employee(id);
+            return employee;
         }
     }
 }
