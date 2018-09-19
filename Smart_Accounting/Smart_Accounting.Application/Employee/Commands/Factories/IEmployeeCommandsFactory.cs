541b2a24ed8119566e58e409255872026c5944f7
ephrem <ephybayru@gmail.com>
2018-09-19 15:55:59 +0300

    updated employee factory

--- Smart_Accounting/Smart_Accounting.Application/Employee/Commands/Factories/IEmployeeCommandsFactory.cs
+++ Smart_Accounting/Smart_Accounting.Application/Employee/Commands/Factories/IEmployeeCommandsFactory.cs
@@ -3,9 +3,8 @@ using Smart_Accounting.Domain.Employe;
 
 namespace Smart_Accounting.Application.Employee.Commands.Factories {
     public interface IEmployeeCommandsFactory {
-        Employees NewEmployee (NewEmployeeModel data);
-
-        Employees UpdatesEmployee();
-
+        Employees NewEmployee (NewEmployeeModel newEmp);
+        Employees UpdatesEmployee(Employee currentEmployee, UpdatedEmployeeModel employee);
+        EmployeeViewModel EmployeeView(Employee employee);
     }
 }
