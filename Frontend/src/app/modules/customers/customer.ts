72be853597527afa16c06f624b9c2072eff1831d
ephrem <ephybayru@gmail.com>
2018-09-25 15:34:35 +0300

    customers interface

--- Frontend/src/app/modules/customers/customer.ts
+++ Frontend/src/app/modules/customers/customer.ts
@@ -1,2 +1,14 @@
-export interface Customer {
+export class Customer {
+    id: number;
+    Full_Name: string;
+    Account_Number: string;
+    Email: string;
+    Phone_No: string;
+    Country: string;
+    City: string;
+    SubCity: string;
+    House_No: string;
+    Postal_Code: string;
+    Date_Added: Date;
+    Date_Updated: Date;
 }
