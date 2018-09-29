162a2e8bcfa2dd42e21de7c1ac545511d1b5e4af
ephrem <ephybayru@gmail.com>
2018-09-29 09:14:00 +0300

    customers command and command factories

---
+++ Smart_Accounting/Smart_Accounting.Application/Customers/Commands/Factories/CustomerCommandsFactory.cs
@@ -0,0 +1,62 @@
+using Smart_Accounting.Application.Customers.Models;
+using Smart_Accounting.Domain.Customers;
+
+namespace Smart_Accounting.Application.Customers.Commands.Factories
+{
+    public class CustomerCommandsFactory : ICustomerCommandsFactory
+    {
+        public Customer NewCustomer(NewCustomerModel newCustomer)
+        {
+            var customer = new  Customer();
+
+            customer.FullName = newCustomer.FullName;
+            customer.AccountId = newCustomer.AccountId;
+            customer.AccountNumber = newCustomer.AccountNumber;
+            customer.Email = newCustomer.Email;
+            customer.PhoneNo = newCustomer.Phone_No;
+            customer.Country = newCustomer.Country;
+            customer.City = newCustomer.City;
+            customer.SubCity = newCustomer.SubCity;
+            customer.HouseNo = newCustomer.HouseNo;
+            customer.PostalCode = newCustomer.PostalCode;
+
+            return customer;
+        }
+
+
+         public CustomerViewModel CustomerView(Customer customer) {
+             var customers = new CustomerViewModel();
+
+            customer.Id = customer.Id;
+            customer.FullName = customer.FullName;
+            customer.AccountId = customer.AccountId;
+            customer.AccountNumber = customer.AccountNumber;
+            customer.Email = customer.Email;
+            customer.PhoneNo = customer.PhoneNo;
+            customer.Country = customer.Country;
+            customer.City = customer.City;
+            customer.SubCity = customer.SubCity;
+            customer.HouseNo = customer.HouseNo;
+            customer.PostalCode = customer.PostalCode;
+
+            return customers;
+
+         }
+
+        public Customer UpdatesCustomer(Customer currentCustomer, UpdateCustomerModel updateCustomer)
+        {
+            currentCustomer.FullName = currentCustomer.FullName;
+            currentCustomer.AccountId = currentCustomer.AccountId;
+            currentCustomer.AccountNumber = currentCustomer.AccountNumber;
+            currentCustomer.Email = currentCustomer.Email;
+            currentCustomer.PhoneNo = currentCustomer.PhoneNo;
+            currentCustomer.Country = currentCustomer.Country;
+            currentCustomer.City = currentCustomer.City;
+            currentCustomer.SubCity = currentCustomer.SubCity;
+            currentCustomer.HouseNo = currentCustomer.HouseNo;
+            currentCustomer.PostalCode = currentCustomer.PostalCode;
+
+            return currentCustomer;
+        }
+    }
+}
