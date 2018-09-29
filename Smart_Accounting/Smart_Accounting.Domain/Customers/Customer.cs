3d0742230a45ecf5f7c38f8b58e1c93eb89803dd
ephrem <ephybayru@gmail.com>
2018-09-29 09:16:04 +0300

    merge

--- Smart_Accounting/Smart_Accounting.Domain/Customers/Customer.cs
+++ Smart_Accounting/Smart_Accounting.Domain/Customers/Customer.cs
@@ -7,12 +7,18 @@ namespace Smart_Accounting.Domain.Customers
     public partial class Customer
     {
         public uint Id { get; set; }
-        public string Name { get; set; }
+        public string FullName { get; set; }
         public uint AccountId { get; set; }
-        public float CreditLimit { get; set; }
-        public DateTime? DateAdded { get; set; }
-        public DateTime? DateUpdated { get; set; }
-
+        public Int32 AccountNumber { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string SubCity { get; set; }
+        public string HouseNo { get; set; }
+        public string PostalCode { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set;}
         public AccountChart Account { get; set; }
     }
 }
