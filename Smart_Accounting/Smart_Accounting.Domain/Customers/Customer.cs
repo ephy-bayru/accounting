5c9a378700f92682ad5c1246e02cafc5cdb15e67
ephrem <ephybayru@gmail.com>
2018-10-01 11:24:05 +0300

    customer application fixed errors

--- Smart_Accounting/Smart_Accounting.Domain/Customers/Customer.cs
+++ Smart_Accounting/Smart_Accounting.Domain/Customers/Customer.cs
@@ -10,13 +10,13 @@ namespace Smart_Accounting.Domain.Customers
         public string FullName { get; set; }
         public uint AccountId { get; set; }
         public Int32 AccountNumber { get; set; }
-        public int PhoneNo { get; set; }
+        public string PhoneNo { get; set; }
         public string Email { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
         public string SubCity { get; set; }
         public string HouseNo { get; set; }
-        public int PostalCode { get; set; }
+        public string PostalCode { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateUpdated { get; set;}
         public AccountChart Account { get; set; }
