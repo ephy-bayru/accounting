855e1c96a72920a19fa0be1a30de4e46ba5fe58c
ephrem <ephybayru@gmail.com>
2018-10-01 11:30:27 +0300

    merge

--- Smart_Accounting/Smart_Accounting.Domain/Employee/Employees.cs
+++ Smart_Accounting/Smart_Accounting.Domain/Employee/Employees.cs
@@ -9,7 +9,7 @@ namespace Smart_Accounting.Domain.Employe
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public int PhoneNo { get; set; }
+        public string PhoneNo { get; set; }
         public string Gender { get; set; }
         public string Password { get; set; }
         public DateTime BirthDate { get; set; }
