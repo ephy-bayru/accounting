aeed93989ca8d654b1b820b1b95fa20cdbaed27f
ephrem <ephybayru@gmail.com>
2018-10-03 14:56:06 +0300

    suppliers queries

--- Smart_Accounting/Smart_Accounting.Application/Supplier/Queries/SupplierQuerieies.cs
+++ Smart_Accounting/Smart_Accounting.Application/Supplier/Queries/SupplierQuerieies.cs
@@ -1,16 +1,35 @@
-using System.Collections.Generic;
+using Smart_Accounting.Application.Supplier.Commands.Factories;
 using Smart_Accounting.Application.Supplier.Interfaces;
-using Smart_Accounting.Domain;
+using Smart_Accounting.Application.Interfaces;
 using Smart_Accounting.Domain.Supplier;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Smart_Accounting.Application.Supplier.Queries {
-    public class SuppliersQuery : ISuppliersQuery {
-        public IEnumerable<Suppliers> GetAll () {
-            throw new System.NotImplementedException ();
+namespace Smart_Accounting.Application.Supplier.Queries
+{
+    public class SuppliersQuery : ISuppliersQuery
+    {
+        private readonly IAccountingDatabaseService _database;
+        public ISupplierCommandsFactory _factory;
+
+        public SuppliersQuery(
+            IAccountingDatabaseService database,
+            ISupplierCommandsFactory factory
+         )
+        {
+            _database = database;
+            _factory = factory;
+        }
+        public IEnumerable<Suppliers> GetAll()
+        {
+            return _database.Suppliers.ToList();
         }
 
-        public Suppliers GetById (uint supplierId) {
-            throw new System.NotImplementedException ();
+        public Suppliers GetById(uint id)
+        {
+            var supplier = _database.Suppliers.Find(id);
+            return supplier;
         }
     }
 }
