982b70e9651d9b60e6e8236d96b2de0ad2de9aad
ephrem <ephybayru@gmail.com>
2018-10-03 14:57:00 +0300

    suppliers domain

--- Smart_Accounting/Smart_Accounting.Domain/Supplier/Suppliers.cs
+++ Smart_Accounting/Smart_Accounting.Domain/Supplier/Suppliers.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using Smart_Accounting.Domain.AccountCharts;
 
-namespace Smart_Accounting.Domain.Supplier {
-    public partial class Suppliers {
-        public int SupplierId { get; set; }
-        public string Name { get; set; }
-        public string Discription { get; set; }
-        public sbyte TaxIncluded { get; set; }
-        public int? CreditLimit { get; set; }
-        public string BankAccount { get; set; }
-        public sbyte Active { get; set; }
+namespace Smart_Accounting.Domain.Supplier
+{
+    public partial class Suppliers
+    {
+        public uint Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
         public string AccountId { get; set; }
-
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string SubCity { get; set; }
+        public string HouseNo { get; set; }
+        public string PostalCode { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
         public AccountChart Account { get; set; }
     }
 }
