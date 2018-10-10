cb015c0b1bc64be7e6c08fd7d269818e672a406b
Mikael Araya <Mikaelaraya12@gmail.com>
2018-10-10 11:42:30 +0300

    #382 optimized customer query

--- Smart_Accounting/Smart_Accounting.Application/Customers/Interfaces/ICustomerQuery.cs
+++ Smart_Accounting/Smart_Accounting.Application/Customers/Interfaces/ICustomerQuery.cs
@@ -3,17 +3,17 @@
  * @Author:  Mikael Araya
  * @Contact: MikaelAraya12@gmail.com
  * @Last Modified By:  Mikael Araya
- * @Last Modified Time: Oct 10, 2018 9:16 AM
+ * @Last Modified Time: Oct 10, 2018 11:03 AM
  * @Description: Modify Here, Please 
  */
 using System.Collections.Generic;
 using Smart_Accounting.Domain.Customers;
 
-namespace Smart_Accounting.Application.Customers.Interfaces
-{
-    public interface ICustomerQuery
-    {
-        Customer GetById(uint id);
-         IEnumerable<Customer> GetAll();
+namespace Smart_Accounting.Application.Customers.Interfaces {
+    public interface ICustomerQuery {
+        Customer GetById (uint id);
+        IEnumerable<CustomerAccount> GetAllCustomerAccounts ();
+        CustomerAccount GetCustomerAccountById (uint id);
+        IEnumerable<Customer> GetAll ();
     }
 }
